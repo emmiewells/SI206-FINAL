7064cf6aaf5404c00285507a33a9294226469033
emmiewells <70598315+emmiewells@users.noreply.github.com>
2020-12-01 22:52:21 -0500

    start visualizations

--- src/visualizations.py
+++ src/visualizations.py
@@ -1,1 +1,11 @@
-import plotly.graph_objects as go 
+import plotly.graph_objects
+import plotly.express as px
+import json
+
+
+def main():
+  
+    
+#create a bar chart displaying the comparision of male and females cases in the us 
+
+# create a pie chart showing which pre-exisitng health conditions affected the amount of deaths 
