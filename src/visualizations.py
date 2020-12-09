9cd94298f90f9fa68ad4bd4496318cd0780a22f6
Michelle Xie <mimixie@Michelles-MacBook-Pro.local>
2020-12-09 18:17:04 -0500

    message

--- src/visualizations.py
+++ src/visualizations.py
@@ -1,22 +1,27 @@
 import plotly.graph_objects
-import plotly.express as px
-import json
+# import plotly.express as px
+# import json
 
 
-def main():
+# def main():
 
-    # create a bar chart displaying the comparision of male and females cases in the us.
+#     # create a bar chart displaying the comparision of male and females cases in the us.
 
-    # create a pie chart showing how COVID-19 deaths compares to pre-existing health conditions deaths.
+#     # create a pie chart showing how COVID-19 deaths compares to pre-existing health conditions deaths.
 
-    # create a scatter plot of the age groups that have contracted covid-19.
+#     # create a scatter plot of the age groups that have contracted covid-19.
 
-    pass
+#     pass
 
 # import plotly.graph_objects as go 
-# label_list = ['240213',  ]
-# value_list = [100, 200]
-# fig = go.Figure(data=[go.Pie(labels=label_list, values=value_list)])
+# death_causes_list = ['COVID19_Deaths', 'Pneumonia_Deaths', 'Influenza_Deaths']
+# amount_list = [240213, 242296, 6829]
+# fig = go.Figure(data=[go.Pie(labels=death_causes_list, values=amount_list)])
 # fig.show()
-if __name__ == '__main__':
-    main()
+
+# import plotly.express as px
+# age_group = ('Under 1 year', '0-17 years', '1-4 years', '5-14 years', '15-24 years', '18-29 years', '25-34 years', '30-49 years', '35-44 years', '45-54 years', '50-64 years', '55-64 years', '65-74 years', '75-84 years', '85 years and over')
+# Covid19_Deaths = 
+
+# if __name__ == '__main__':
+#     main()
