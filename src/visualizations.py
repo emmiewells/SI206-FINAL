00fd2a48055eee48120ca7636adefaf576eb0bdf
xiemichelle <49491397+xiemichelle@users.noreply.github.com>
2020-12-09 13:49:48 -0500

    Update visualizations.py

--- src/visualizations.py
+++ src/visualizations.py
@@ -5,11 +5,11 @@ import json
 
 def main():
 
-    # create a bar chart displaying the comparision of male and females cases in the us
+    # create a bar chart displaying the comparision of male and females cases in the us.
 
-    # create a pie chart showing which pre-existing health conditions affected the amount of deaths
+    # create a pie chart showing which pre-existing health conditions affected the amount of deaths.
 
-    # create a scatter plot of the age groups that have contracted covid-19
+    # create a scatter plot of the age groups that have contracted covid-19.
 
     pass
 
