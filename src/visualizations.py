c9c759460947e7af83adf8ade5f4eb9b662c19a2
Lionel Robert <lionelr@umich.edu>
2020-12-08 23:01:22 -0500

    formatting

--- src/visualizations.py
+++ src/visualizations.py
@@ -8,7 +8,7 @@ def main():
     # create a bar chart displaying the comparision of male and females cases in the us
 
     # create a pie chart showing which pre-existing health conditions affected the amount of deaths
-    
+
     # create a scatter plot of the age groups that have contracted covid-19
 
     pass
